39819f5944e500380a0e217f2b56f690a8909692
John Barber <jsbarber@gmail.com>
2016-06-13 15:15:14 +0100

    Updated dev/web-build.sh - fully tested deployment in dev - size=2

--- dev/web_data.sh
+++ dev/web_data.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -v
 yum install -y httpd
+#Observed failure of initial yum install with a timeout (possibly caused by NAT Gateway not being up yet
+if [ $? -ne 0 ]; then 
+	sleep 30;
+	yum install -y httpd
+fi
 service iptables stop
 chkconfig --level 235 iptables stop
+echo "MyApp-DEV-Test" > /var/www/html/index.html
+chown apache:apache /var/www/html/index.html
 service httpd start
